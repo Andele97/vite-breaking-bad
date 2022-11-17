dd9cde89ef5cde6d1dfdb2188a2691150b2122bc
Andele97 <andreapinna40@live.it>
2022-11-17 17:24:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue'
+import AppSearch from './components/AppSearch.vue'
+import AppMain from './components/AppMain.vue'
+import CharacterList from './components/CharacterList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppSearch,
+    AppMain,
+    CharacterList
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppSearch />
+  <AppMain />
+</template>
+
+<style lang="scss">
+
+  @use './styles/general';
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<template>
+  
+    <div class="container-fluid">
+      <header>
+        <div class="logo">
+          <img src="../assets/img/Breaking_Bad_logo.svg" alt="">
+        </div>
+        <h1></h1>
+      </header>        
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+  header {
+    height: 60px;
+    padding: 15px;
+    .logo{
+      height: 60px;
+      img{
+        height: 100%;
+      }
+    }
+  }
+
+</style>
